6162ab07347b99cb4459c0c27e7ab9b5a638d371
Okinbird <okinyinka@yahoo.co.uk>
2023-05-25 11:24:07 -0700

    Connect to secure database - et_db function that returns a connector to the database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -75,14 +75,17 @@ def get_logger() -> logging.Logger:
 
     return log
 
+
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Description: Implement a get_db function that returns a connector to the database 
-                     (mysql.connector.connection.MySQLConnection object). """
+    """ Description: Implement a get_db function that returns a connector
+                     to the database
+                     (mysql.connector.connection.MySQLConnection object).
+    """
     connection_db = mysql.connector.connection.MySQLConnection(
-        username = getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password = getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host = getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database = getenv('PERSONAL_DATA_DB_NAME')
+        username=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME')
     )
 
     return connection_db
