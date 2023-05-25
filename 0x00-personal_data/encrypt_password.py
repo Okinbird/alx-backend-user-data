cc4f33912160ac65fd3e03fb622d3417f2d202f0
Okinbird <okinyinka@yahoo.co.uk>
2023-05-25 12:06:07 -0700

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
    5. Encrypting passwords - hash_password
+   6. Check valid password - is_valid
 """
 import bcrypt
 
@@ -16,3 +17,17 @@ def hash_password(password: str) -> bytes:
     hashed_pass = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
 
     return hashed_pass
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Description: Implement an is_valid function that expects 2 arguments and
+                     returns a boolean.
+
+        Arguments:  hashed_password: bytes type
+                    password: string type
+    """
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
