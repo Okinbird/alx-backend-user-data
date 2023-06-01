6396d06b1dd14164f03aeaab99589f8f50736668
Okinbird <okinyinka@yahoo.co.uk>
2023-06-01 13:37:30 -0700

    returns a cookie value from a request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -34,3 +35,12 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Validates current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request """
+        if request is None:
+            return None
+
+        _my_session_id = getenv('SESSION_NAME')
+
+        return request.cookies.get(_my_session_id)
