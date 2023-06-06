4f28ff7e8c44466c54656ca3b4a6015a3761fdff
Okinbird <okinyinka@yahoo.co.uk>
2023-06-06 14:48:34 -0700

    save the user to the database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -34,7 +34,7 @@ class DB:
         """ Save user to the database
         Return: User Object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
         return user
