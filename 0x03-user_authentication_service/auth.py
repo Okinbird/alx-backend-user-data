501d40234aeee00cb725dfe2564ae5d64e434f21
Okinbird <okinyinka@yahoo.co.uk>
2023-06-07 10:58:54 -0700

    returns the session ID as a string

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,3 +48,14 @@ class Auth:
             return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Returns the session ID as a string """
+        try:
+            db_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(db_user.id, session_id=session_id)
+        return session_id
