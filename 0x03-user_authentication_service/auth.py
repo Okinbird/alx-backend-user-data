82b3c02563aac45823f352de9e7fbd1bcd62616a
Okinbird <okinyinka@yahoo.co.uk>
2023-06-07 09:16:52 -0700

    return a string representation of a new UUID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,11 +6,18 @@ from user import Base, User
 from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     return hashpw(password.encode('utf-8'), gensalt())
+
+def _generate_uuid() -> str:
+    """ Generates UUID
+    Returns string representation of a new UUID
+    """
+    return str(uuid4())
 
 
 class Auth:
